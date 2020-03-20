b1cf708d1ea36a29581d719b0d5995e50f083ee5
RampsosPeck <peralta.jorge.uatf@gmail.com>
2020-03-19 22:55:56 -0400

    Finish lesson 3 config class 21 PERFECT

---
+++ app.js
@@ -0,0 +1,26 @@
+import express  from 'express';
+import morgan  from 'morgan';
+import cors  from 'cors';
+import path  from 'path';
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Rutas de la app
+/*app.get('/',  (req, res) => {
+	res.send('Hola mundo Jorge!');
+});*/
+
+// Middleware para vue.js router mode history
+const history = require('connect-history-api-fallback')
+app.use(history())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('puerto', process.env.PORT || 3000)
+app.listen(app.get('puerto'), () => {
+	console.log('Servidor corriendo en: ', app.get('puerto'))
+})
